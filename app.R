64bf24ae0e6dd8487085058c8118fbeb00e39ac5
Jeremy Weiss <jlweiss@email.arizona.edu>
2025-11-17 14:11:00 -0700

    update notification specs

--- app.R
+++ app.R
@@ -51,14 +51,18 @@ server <- function(input, output, session) {
     idRetrievingHourlyData <- shiny::showNotification(
       ui = "Retrieving hourly API data . . .",
       action = NULL,
-      duration = NULL,
+      duration = 3,
       closeButton = FALSE,
       id = "idRetrievingHourlyData",
-      type = "message"
+      type = "message",
+      session = getDefaultReactiveDomain()
     )
 
     on.exit(
-      shiny::removeNotification(id = idRetrievingHourlyData),
+      shiny::removeNotification(
+        id = idRetrievingHourlyData,
+        session = getDefaultReactiveDomain()
+      ),
       add = TRUE
     )
 
@@ -72,14 +76,18 @@ server <- function(input, output, session) {
     idRetrievingHourlyData <- shiny::showNotification(
       ui = "Retrieving hourly Legacy data . . .",
       action = NULL,
-      duration = NULL,
+      duration = 3,
       closeButton = FALSE,
       id = "idRetrievingHourlyData",
-      type = "message"
+      type = "message",
+      session = getDefaultReactiveDomain()
     )
-
+    
     on.exit(
-      shiny::removeNotification(id = idRetrievingHourlyData),
+      shiny::removeNotification(
+        id = idRetrievingHourlyData,
+        session = getDefaultReactiveDomain()
+      ),
       add = TRUE
     )
 
@@ -93,14 +101,18 @@ server <- function(input, output, session) {
     idRetrievingHourlyData <- shiny::showNotification(
       ui = "Joining hourly Legacy and API data . . .",
       action = NULL,
-      duration = NULL,
+      duration = 3,
       closeButton = FALSE,
       id = "idRetrievingHourlyData",
-      type = "message"
+      type = "message",
+      session = getDefaultReactiveDomain()
     )
-
+    
     on.exit(
-      shiny::removeNotification(id = idRetrievingHourlyData),
+      shiny::removeNotification(
+        id = idRetrievingHourlyData,
+        session = getDefaultReactiveDomain()
+      ),
       add = TRUE
     )
 
@@ -119,11 +131,17 @@ server <- function(input, output, session) {
   })
   
   reportingTitle <- shiny::eventReactive(input$retrieveHourlyData, {
-    fxn_reportingTitle(azmetStation = input$azmetStation)
+    fxn_reportingTitle(
+      azmetStation = input$azmetStation,
+      year = input$year
+    )
   })
   
   scatterplotTitle <- shiny::eventReactive(input$retrieveHourlyData, {
-    fxn_scatterplotTitle(azmetStation = input$azmetStation)
+    fxn_scatterplotTitle(
+      azmetStation = input$azmetStation,
+      year = input$year
+    )
   })
   
   
